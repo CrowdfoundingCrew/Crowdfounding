624566ba6ee33974654e1def18fc1a882a03e3df
Francesco Minei <francesco.minei.studenti@isii.it>
2021-04-03 12:10:46 +0200

    bug fix

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@ function GetDBConfig()
 {
 	return array(
 	'hostname' => '127.0.0.1',
-	'username' => 'DonateFor',
-	'password' => '@password1234',
+	'username' => 'AdminDonateFor',
+	'password' => 'password',
 	'dbname' => 'donatefor'
 	);
 }
--- public/logout.php
+++ public/logout.php
@@ -2,6 +2,6 @@
 session_start();
 $_SESSION = array();
 session_destroy();
-header("Location: /public/");
+header("Location: ../public/index.php");
 die();
 ?>
