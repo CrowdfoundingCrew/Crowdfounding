fe314f2f0154996c4a4b9573ad80decbb5fc2233
Francesco Minei <francesco.minei.studenti@isii.it>
2021-04-25 15:49:24 +0200

    navbar link fix

--- public/navbar.php
+++ public/navbar.php
@@ -10,42 +10,42 @@
                 <div class="right"></div>
             </div>
             <li class="nav-item <?=basename($_SERVER['PHP_SELF'])=='index.php'? 'active':''?>">
-                    <a class="nav-link <?=basename($_SERVER['PHP_SELF'])=='index.php'? 'active':''?>" href="/public/index.php">Homepage</a>
+                    <a class="nav-link <?=basename($_SERVER['PHP_SELF'])=='index.php'? 'active':''?>" href="../public/index.php">Homepage</a>
                 </li>
                 <li class="nav-item <?=basename($_SERVER['PHP_SELF'])=='allprojects.php'|| basename($_SERVER['PHP_SELF'])=='project.php'? 'active':''?>">
-                    <a class="nav-link <?=basename($_SERVER['PHP_SELF'])=='allprojects.php'? 'active':''?>" href="/public/allprojects.php">Tutti i progetti</a>
+                    <a class="nav-link <?=basename($_SERVER['PHP_SELF'])=='allprojects.php'? 'active':''?>" href="../public/allprojects.php">Tutti i progetti</a>
                 </li>
                 <li class="nav-item <?=basename($_SERVER['PHP_SELF'])=='aboutus.php'? 'active':''?>">
-                    <a class="nav-link <?=basename($_SERVER['PHP_SELF'])=='aboutus.php'? 'active':''?>" href="/public/aboutus.php">About us</a>
+                    <a class="nav-link <?=basename($_SERVER['PHP_SELF'])=='aboutus.php'? 'active':''?>" href="../public/aboutus.php">About us</a>
                 </li>
                 <li class="nav-item <?=basename($_SERVER['PHP_SELF'])=='contactus.php'? 'active':''?>">
-                    <a class="nav-link <?=basename($_SERVER['PHP_SELF'])=='contactus.php'? 'active':''?>" href="/public/contactus.php">Contact us</a>
+                    <a class="nav-link <?=basename($_SERVER['PHP_SELF'])=='contactus.php'? 'active':''?>" href="../public/contactus.php">Contact us</a>
                 </li>
             <?php if (!isset($_SESSION['Tipo'])) {
             ?>
                 <li class="nav-item <?=basename($_SERVER['PHP_SELF'])=='login.php'|| basename($_SERVER['PHP_SELF'])=='reg.php'? 'active':''?>">
-                    <a class="nav-link <?=basename($_SERVER['PHP_SELF'])=='login.php'|| basename($_SERVER['PHP_SELF'])=='reg.php'? 'active':''?>" href="login.php">Login <i class="fas fa-sign-in-alt"></i></a>
+                    <a class="nav-link <?=basename($_SERVER['PHP_SELF'])=='login.php'|| basename($_SERVER['PHP_SELF'])=='reg.php'? 'active':''?>" href="./login.php">Login <i class="fas fa-sign-in-alt"></i></a>
                 </li>
                 <?php } else if ($_SESSION['Tipo'] === 0) { ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="/donatori/profile.php">Impostazioni account</a>
+                    <a class="nav-link" href="../donatori/profile.php">Impostazioni account</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="logout.php"><?= $_SESSION['Username'] ?> <i class="fas fa-sign-out-alt"></i></a>
+                    <a class="nav-link" href="./logout.php"><?= $_SESSION['Username'] ?> <i class="fas fa-sign-out-alt"></i></a>
                 </li>
             <?php } else if ($_SESSION['Tipo'] === 1) { ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="/onlus/profile.php">Impostazioni account</a>
+                    <a class="nav-link" href="../onlus/profile.php">Impostazioni account</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="logout.php"><?= $_SESSION['Username'] ?> <i class="fas fa-sign-out-alt"></i></a>
+                    <a class="nav-link" href="./logout.php"><?= $_SESSION['Username'] ?> <i class="fas fa-sign-out-alt"></i></a>
                 </li>
             <?php } else if ($_SESSION['Tipo'] === 2) { ?>
                 <li class="nav-item">
-                    <a class="nav-link" href="/admin/allonlus.php">Impostazioni account</a>
+                    <a class="nav-link" href="../admin/allonlus.php">Impostazioni account</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="logout.php"><?= $_SESSION['Username'] ?> <i class="fas fa-sign-out-alt"></i></a>
+                    <a class="nav-link" href="./logout.php"><?= $_SESSION['Username'] ?> <i class="fas fa-sign-out-alt"></i></a>
                 </li>
             <?php } ?>
         </ul>
